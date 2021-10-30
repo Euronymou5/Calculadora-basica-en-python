a5f7c23afbfd4c14e52c3d12f73ee44531ef1223
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-10-29 23:23:14 -0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,60 @@
+def sumar():
+  print('Sumar')
+  numero = int(input('Que numero quieres sumar por otro numero: '))
+  suma = int(input('Ingresa numero a sumar: '))
+  suma = print(numero + suma)
+
+def restar():
+  print('Restar')
+  numero = int(input('Que numero quieres restar por otro numero: '))
+  restar = int(input('Ingresa numero a restar: '))
+  restar = print(numero - restar)
+
+def multi():
+  print('Multiplicar')
+  numero = int(input('Que numero quieres multiplicar por otro numero: '))
+  multiplicar = int(input('Ingresa numero a multiplicar: '))
+  multiplicar = print(numero * multiplicar)
+
+def divir():
+  print('Division')
+  numero = int(input('Ingresa que numero quieres divir por otro numero: '))
+  divir = int(input('Ingresa numero a divir: '))
+  divir = print(numero / divir)
+
+def divison2():
+  print('Division sin flotante')
+  numero = int(input('Ingresa que numero a divir sin flotante: '))
+  divir = int(input('Ingresa numero a divir sin flotante: '))
+  divir = print(numero // divir)
+
+def potencia():
+  print('Calcular Potencias')
+  numero = int(input('Ingresa el numero: '))
+  potencia = int(input('Ingresa el segundo numero: '))
+  potencia = print(numero ** potencia)
+
+def menu():
+  print('[1] Sumar')
+  print('[2] Restar')
+  print('[3] Multiplicar')
+  print('[4] Division Clasica')
+  print('[5] Division sin flotante')
+  print('[6] Calcular Potencias')
+
+menu()
+elegir = int(input('Seleccione una opcion: '))
+if elegir == 1:
+  sumar()
+elif elegir == 2:
+  restar()
+elif elegir == 3:
+  multi()
+elif elegir == 4:
+  divir()
+elif elegir == 5:
+  divison2()
+elif elegir == 6:
+  potencia()
+else: 
+  print('Error opcion invalida.')
