2ab8e76a0f1ac8b43e876e1485194b1385babfbd
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-10-29 23:34:41 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,34 +1,40 @@
 def sumar():
+  print(' ')
   print('Sumar')
   numero = int(input('Que numero quieres sumar por otro numero: '))
   suma = int(input('Ingresa numero a sumar: '))
   suma = print(numero + suma)
 
 def restar():
+  print(' ')
   print('Restar')
   numero = int(input('Que numero quieres restar por otro numero: '))
   restar = int(input('Ingresa numero a restar: '))
   restar = print(numero - restar)
 
 def multi():
+  print(' ')
   print('Multiplicar')
   numero = int(input('Que numero quieres multiplicar por otro numero: '))
   multiplicar = int(input('Ingresa numero a multiplicar: '))
   multiplicar = print(numero * multiplicar)
 
 def divir():
+  print(' ')
   print('Division')
   numero = int(input('Ingresa que numero quieres divir por otro numero: '))
   divir = int(input('Ingresa numero a divir: '))
   divir = print(numero / divir)
 
 def divison2():
+  print(' ')
   print('Division sin flotante')
   numero = int(input('Ingresa que numero a divir sin flotante: '))
   divir = int(input('Ingresa numero a divir sin flotante: '))
   divir = print(numero // divir)
 
 def potencia():
+  print(' ')
   print('Calcular Potencias')
   numero = int(input('Ingresa el numero: '))
   potencia = int(input('Ingresa el segundo numero: '))
